8a8ff3b0512190b487594b4a709fdd6b510ca4a9
vortex <38843963+vortexreanimated@users.noreply.github.com>
2024-03-02 18:51:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## mangrove
+Derivative of [mache](https://github.com/PaperMC/mache/tree/) that provides patches compatible
+with Grove's build tooling.
+
+### Goal
+Mangrove is a project that provides fully compatible base patches for the vanilla
+minecraft server. This serves as the base for the Grove Minecraft server project. Of course,
+anyone can use this for their own purposes as well.
+
+### License
+All patches under this license are in [LGPL-3.0-only](https://www.gnu.org/licenses/lgpl-3.0-standalone.html), in the hopes
+that they will be useful. All the patches' base code belongs to Mojang, and are not part of the licensed work.
---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    `maven-publish`
+}
+
+val patchesVersion = "1.20.4"
+
+val generateMangroveZip by tasks.registering(Zip::class) {
+    group = "mangrove"
+    description = "Creates the Mangrove artifact for publishing."
+
+    into("patches") {
+        from(layout.projectDirectory.dir("patches"))
+    }
+
+    archiveBaseName.set("mangrove")
+    archiveVersion.set(patchesVersion)
+    archiveExtension.set("zip")
+}
+
+val mangroveArtifact = artifacts.add("mangrove", generateMangroveZip)
+
+publishing {
+    publications {
+        register<MavenPublication>("mangrove") {
+            groupId = "com.grovepowered.mangrove"
+            artifactId = "mangrove"
+            version = patchesVersion
+
+            artifact(mangroveArtifact)
+        }
+    }
+
+    repositories {
+        maven("https://repo.rubygame.net/repository/maven-snapshots/") {
+            name = "RubyGame"
+            credentials {
+                username = "admin"
+                password = System.getProperty("SONATYPE_PASSWORD")
+            }
+        }
+    }
+}
